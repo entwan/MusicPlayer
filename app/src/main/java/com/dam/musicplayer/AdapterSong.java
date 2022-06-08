ce4d8860f2ad236c1f86d2f1bb89132fb49a973c
entwan <entwan@gmail.com>
2022-06-08 16:13:19 +0200

    V2 1ere version qui affiche la liste des chansons

--- app/src/main/java/com/dam/musicplayer/AdapterSong.java
+++ app/src/main/java/com/dam/musicplayer/AdapterSong.java
@@ -1,5 +1,8 @@
 package com.dam.musicplayer;
 
+import android.content.Context;
+import android.net.Uri;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -8,22 +11,74 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
+
+import java.util.ArrayList;
+
 public class AdapterSong extends RecyclerView.Adapter<AdapterSong.MyViewHolder> {
+
+    //1 Vars
+    private Context context;
+    private ArrayList<ModelSong> songArrayList;
+
+    // 2 Constructeurs
+    public AdapterSong(Context context, ArrayList<ModelSong> songArrayList) {
+        this.context = context;
+        this.songArrayList = songArrayList;
+    }
+
+    // La gestion du click
+    public OnItemClickListener onItemClickListener;
+
 
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        return null;
+        // 3 Creation du Layout inflater
+        View view = LayoutInflater.from(context).inflate(R.layout.item_song, parent,false);
+
+        return new MyViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull MyViewHolder holder, int position) {
+        // 4 Remplissage du DataBinding
+        holder.title.setText(songArrayList.get(position).getSongTitle());
+        holder.artist.setText(songArrayList.get(position).getSongArtist());
+
+        // Gestion de l'image
+        Uri imgUri = songArrayList.get(position).getSongCover();
+
+        /* utilisation de Glide pour la gestion des images */
+        Context context = holder.cover.getContext();
+
+        //methode ultra basique
+//        Glide.with(context)
+//            // on load l'image depuis le chemin vers le dossier de stockage des covers
+//            .load(imgUri)
+//            .into(holder.cover);
+
+        RequestOptions options = new RequestOptions()
+                .centerCrop()
+                .error(R.drawable.ic_note_24_w)
+                .placeholder(R.drawable.ic_note_24_w);
+
+        // methode normale
+        Glide.with(context)
+                .load(imgUri)
+                .apply(options)
+                .fitCenter()
+                .override(150,150)
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(holder.cover);
 
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return songArrayList.size();
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
@@ -36,6 +91,22 @@ public class AdapterSong extends RecyclerView.Adapter<AdapterSong.MyViewHolder> 
             artist = itemView.findViewById(R.id.tvArtist);
             cover = itemView.findViewById(R.id.ivCover);
 
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    onItemClickListener.onItemClick(getAdapterPosition(), v);
+                }
+            });
+
+
         }
+    }
+
+    public interface OnItemClickListener {
+        void onItemClick(int position, View view);
+    }
+
+    public void setOnItemClickListener(OnItemClickListener onItemClickListener) {
+        this.onItemClickListener = onItemClickListener;
     }
 }
--- app/src/main/java/com/dam/musicplayer/MainActivity.java
+++ app/src/main/java/com/dam/musicplayer/MainActivity.java
@@ -1,14 +1,32 @@
 package com.dam.musicplayer;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.DividerItemDecoration;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.ContentUris;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
 import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import java.util.ArrayList;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -16,10 +34,137 @@ public class MainActivity extends AppCompatActivity {
 
     //debut version v2
     private static final String TAG = "MainActivity";
-    /** Ajout automatique de la lecture **/
-    MediaPlayer mediaPlayer = new MediaPlayer();
 
-    /** Méthodes pour le fonctionnement de l'application **/
+
+    /**
+     * Ajout automatique de la lecture
+     **/
+    private MediaPlayer mediaPlayer;
+
+    private SeekBar sbPosition;
+
+    private TextView tvCurrentPos, tvTotalDuration, tvSongTitle;
+
+    private ImageView btnPrev, btnPlay, btnNext;
+
+    private RecyclerView recyclerView;
+
+    private ArrayList<ModelSong> songArrayList;
+
+    public static final int PERMISSION_READ = 0;
+
+
+    // Methode d'initialisation
+    private void init() {
+        // init UI
+        sbPosition = findViewById(R.id.sbPosition);
+        tvCurrentPos = findViewById(R.id.tvCurrentPos);
+        tvTotalDuration = findViewById(R.id.tvTotalDuration);
+        tvSongTitle = findViewById(R.id.tvSongTitle);
+        btnPrev = findViewById(R.id.iv_btnPrev);
+        btnPlay = findViewById(R.id.iv_btnPlay);
+        btnNext = findViewById(R.id.iv_btnNext);
+        recyclerView = findViewById(R.id.recyclerView);
+
+        // init
+        mediaPlayer = new MediaPlayer();
+
+        songArrayList = new ArrayList<>();
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this,
+                LinearLayoutManager.VERTICAL, false));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+
+    }
+
+
+    /**
+     * Méthodes pour le fonctionnement de l'application
+     **/
+    // Methode pour verifier les permissions de l'application
+    public boolean checkPermission() {
+        int READ_INTERNAL_PERMISSION = ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE);
+        if (READ_INTERNAL_PERMISSION != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{
+                    Manifest.permission.READ_EXTERNAL_STORAGE}, PERMISSION_READ);
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            case PERMISSION_READ: {
+                if (grantResults.length > 0 && permissions[0].equals(Manifest.permission.READ_EXTERNAL_STORAGE)) {
+                    if (grantResults[0] == PackageManager.PERMISSION_DENIED) {
+                        Toast.makeText(this, "Please allow storage access permission", Toast.LENGTH_SHORT).show();
+                    } else {
+                        // Lancement de l'app
+                        setSong();
+                    }
+                }
+            }
+        }
+    }
+
+    public void getAudioFiles() {
+        ContentResolver contentResolver = getContentResolver();
+
+        Uri uri = MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+
+        Cursor cursor = contentResolver.query(uri, null, null, null, null);
+
+        // loop au travers de toutes les lignes et ajout dans le ArrayList
+        if (cursor != null && cursor.moveToFirst()) {
+            do {
+                // Récuperation des donnees pour injection dans le tableau
+                String title = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.TITLE));
+                String artist = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.ARTIST));
+                String duration = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DURATION));
+                String url = cursor.getString(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DATA));
+                long album_id = cursor.getLong(cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.ALBUM_ID));
+
+                Uri coverFolder = Uri.parse("content://media/external/audio/albumart");
+                Uri albumArtUri = ContentUris.withAppendedId(coverFolder, album_id);
+
+                //Utilisation du model pour remplir les valeurs dans le tableau
+                ModelSong modelSong = new ModelSong();
+                modelSong.setSongTitle(title);
+                modelSong.setSongArtist(artist);
+                modelSong.setSongDuration(duration);
+                modelSong.setSongCover(albumArtUri);
+                modelSong.setSongUri(Uri.parse(url));
+
+                // Ajout de ces données dans le ArrayList
+                songArrayList.add(modelSong);
+
+            } while (cursor.moveToNext());
+        }
+
+        // Ajpout de l'adapter qui va permettre l'affichage des donnees recuperees dans le ArrayList
+        AdapterSong adapterSong = new AdapterSong(this,songArrayList);
+
+        // Adaptation des donnees de ArrayList au recycler
+        recyclerView.setAdapter(adapterSong);
+
+        adapterSong.setOnItemClickListener(new AdapterSong.OnItemClickListener() {
+            @Override
+            public void onItemClick(int position, View view) {
+                // a venir
+            }
+        });
+
+
+
+
+    }
+
+    private void setSong() {
+        //getAudioFiles();
+    }
+
     public void play(View view) {
         mediaPlayer.start();
         Log.i(TAG, "play");
@@ -31,44 +176,8 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-    private void volume(){
-        // Association de seekbar au Java
-        SeekBar sbVolume = findViewById(R.id.sbVolume);
 
-        // Initialiser le manager en tant que service
-        AudioManager audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
-
-        // Volume max du terminal
-        int volumeMax = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
-
-        // Valorisation de cette valeur au max de la seekbar
-        sbVolume.setMax(volumeMax);
-
-        // Définition du volume courant
-        int currentVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
-
-        sbVolume.setProgress(currentVolume);
-
-        sbVolume.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                Log.i(TAG, "onProgressChanged: Volume = " + Integer.toString(progress));
-                audioManager.setStreamVolume(AudioManager.STREAM_MUSIC,progress,0);
-            }
-
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-            }
-
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-
-            }
-        });
-
-    }
-
-    private void position(){
+    private void position() {
         // Association de seekbar au Java
         SeekBar sbPosition = findViewById(R.id.sbPosition);
 
@@ -105,28 +214,37 @@ public class MainActivity extends AppCompatActivity {
                 // Déplacement automatique
                 sbPosition.setProgress(mediaPlayer.getCurrentPosition());
             }
-        },0,300);
+        }, 0, 300);
 
     }
 
     //END Volume
+
     /************** Cycles de vie **************/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
         /** Methode 1 lecture automatique du son
-        mediaPlayer = MediaPlayer.create(this,R.raw.sound);
-        mediaPlayer.start();
-        **/
+         mediaPlayer = MediaPlayer.create(this,R.raw.sound);
+         mediaPlayer.start();
+         **/
 
         /** Methode 2 avec les boutons **/
-        mediaPlayer = MediaPlayer.create(this,R.raw.sound);
+        //mediaPlayer = MediaPlayer.create(this,R.raw.sound);
+
+
 
         // Lancement de la methode
-        volume();
-        position();
+        //position();
+
+        //en attendant Godot
+
+        init();
+        checkPermission();
+        getAudioFiles();
 
     }
 
